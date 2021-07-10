47c37b86c575d352a441cd0d0967fa2f36ab40eb
Robin Nicole <robin@192.168.1.112>
2021-07-10 18:41:37 +0100

    process the subtitles in the backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,44 @@
-from typing import Optional
+from typing import Optional, List
 from fastapi import FastAPI, HTTPException
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api import TranscriptsDisabled
-
+from functools import lru_cache
+from .utils import parse_output
 app = FastAPI()
 
-@app.get("/video_transcript/{video_id}")
-def read_item(video_id: str, q: Optional[str] = None):
+@lru_cache(maxsize=None)
+def cached_transcript(video_id: str):
+    """
+    Caching layer on top of the youtube subtitles api
+    """
+    return YouTubeTranscriptApi.get_transcript(video_id)
+
+
+@app.get("/video_transcript/raw/{video_id}")
+def raw_transcript(video_id: str, q: Optional[str] = None):
+    """
+    Interface to download the raw transcript from youtube
+    video
+    """
     try:
         return {
             'video_id': video_id,
-            'transcript': YouTubeTranscriptApi.get_transcript(video_id)
+            'transcript': cached_transcript(video_id)
         }
     except TranscriptsDisabled as e:
         raise HTTPException(status_code=500, detail=e.__str__())
+
+@app.get("/video_transcript/processed/{video_id}/{words_list}")
+def process_transcript(video_id: str, words_list: str):
+    """
+    Endpoint to retrieve the part of the transcript which contain one of the words supplied in the 
+    words_list
+    Input:
+    video_id: the id of the video we want to check
+    words_list: a coma separated list of the words that need to appear
+    """
+    words = [word.lower() for word in words_list.split(',')]
+    transcript = cached_transcript(video_id)
+    return {'video_id': video_id,
+            'parsed_transcript': parse_output(transcript, words)
+    }
---
+++ backend/app/utils.py
@@ -0,0 +1,20 @@
+
+from typing import List
+from copy import deepcopy
+
+def parse_output(transcript: dict, words: List[str]):
+    """
+    Parse a transcript and only keep the part of it which contain the words 
+    in the words list
+    """
+    output = []
+    transcript = deepcopy(transcript)
+    for transcript_part in transcript:
+        words_in_transcript_part = [
+            word for word in words
+            if word in transcript_part['text'].lower()
+        ]
+        if words_in_transcript_part:
+            transcript_part['words'] = words_in_transcript_part #words_in_transcript_part
+            output.append(transcript_part)
+    return output
